04615e8b4a086e4bce13567a61aa5283e406b724
kikoki34 <kikoki@tutamail.com>
2024-06-21 17:42:12 +0300

    Improve performance of search

---
+++ src/Layouts/Sidebar/Components/SideMenuLink.js
@@ -0,0 +1,34 @@
+import ArrowExpandIcon from '../../../Icons/ArrowExpandIcon';
+import Clickable from '../../../Components/Clickable';
+import styled from 'styled-components';
+
+function SideMenuLink({name, isActive, icon}) {
+    const Container = styled.div`
+        background-color: ${isActive ? '#DFDFDF' : '#FFF'};
+        padding-top: 12px;
+        padding-bottom: 12px;
+
+        &:hover {
+            opacity: 0.8;
+        }
+    `;
+
+    const TextContainer = styled.div`
+        margin-left: 12px;
+    `;
+
+    if (icon === undefined) {
+        icon = <ArrowExpandIcon/>;
+    }
+    
+    return (
+        <Clickable fillWidth={true}>
+            <Container className="d-flex align-self-center align-items-center justify-content-start px-3">
+                {icon}
+                <TextContainer>{name}</TextContainer>
+            </Container>
+        </Clickable>
+    );
+}
+  
+export default SideMenuLink;
