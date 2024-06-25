7e160b7a51b5fb15ee5760583cb3b8cee172c3da
kikoki34 <kikoki@tutamail.com>
2024-06-25 17:42:12 +0300

    add new tests

---
+++ src/Layouts/Page/Header/Components/Account.js
@@ -0,0 +1,29 @@
+import Clickable from '../../../../Components/Clickable';
+import ArrowDownIcon from '../../../../Icons/ArrowDownIcon';
+import styled from 'styled-components';
+
+const Text = styled.div`
+    color: #D3EAFE;
+`;
+const HoverWrapper = styled.div`
+    display: inherit;
+    &:hover {
+        opacity: 80%;
+    }
+`;
+
+function Account() {
+    return (
+        <Clickable>
+            <div className='d-inline-flex align-items-center align-self-center'>
+                <img src={require('../../../../Images/Avatar.png')}></img>  {/* Аватарка */}
+                <HoverWrapper className='d-inline-flex align-items-center align-self-center'>
+                    <Text className='text-rg-bd ms-3 me-2'>Антон И.</Text>  {/* Имя пользователя */}
+                    <ArrowDownIcon/>
+                </HoverWrapper>
+            </div>
+        </Clickable>
+    );
+}
+
+export default Account;
